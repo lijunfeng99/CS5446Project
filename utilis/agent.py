1dbb278403832e29b919075a6c898a7ff6f2ef62
zhm1211 <62014894+zhm1211@users.noreply.github.com>
2024-11-22 22:14:50 +0800

    Add files via upload

--- utilis/agent.py
+++ utilis/agent.py
@@ -2,6 +2,7 @@ from utilis.Random_agent import RandomAgent
 from utilis.Naive_Qlearning_agent import Naive_Qlearning_agent
 from utilis.Double_Qlearning_agent import Double_Qlearning_agent
 from utilis.Monte_Carlo_learning import MonteCarloAgent as Monte_Carlo_agent
+from utilis.TD3_agent import TD3_agent
 
 class RandomAgent(RandomAgent):
     def __init__(self, name):
@@ -18,3 +19,7 @@ class Double_Qlearning_agent(Double_Qlearning_agent):
 class Monte_Carlo_agent(Monte_Carlo_agent):
     def __init__(self, name):
         super().__init__(name)  # 调用父类的构造函数
+
+class TD3_agent(TD3_agent):
+    def __init__(self, name):
+        super().__init__(name)
